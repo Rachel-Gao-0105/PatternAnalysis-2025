491d403f16781821f38f1a4d4f2382e40e7f05c4
gaoruijia <gaoruijia15@outlook.com>
2025-10-13 19:04:00 +1000

    update dataset.py -- load the dataset

--- recognition/8_ConvNeXt_49085873GaoRuijia/dataset.py
+++ recognition/8_ConvNeXt_49085873GaoRuijia/dataset.py
@@ -1,3 +1,28 @@
-'''
-containing the data loader for loading and preprocessing the data
-'''
+from torchvision import transforms
+
+# === load the dataset ===========================================================
+
+def build_transforms(img_size=224, mean=None, std=None):
+    if mean is None or std is None:
+        mean = [0.485, 0.456, 0.406]
+        std  = [0.229, 0.224, 0.225]
+
+    train_tf = transforms.Compose([
+        transforms.Resize(int(img_size*1.15)),
+        transforms.RandomResizedCrop(img_size, scale=(0.8, 1.0)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomRotation(degrees=10), 
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std),  
+    ])
+
+    test_tf = transforms.Compose([
+        transforms.Resize(int(img_size*1.15)),
+        transforms.CenterCrop(img_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std),
+    ])
+    return train_tf, test_tf
+
+
+
